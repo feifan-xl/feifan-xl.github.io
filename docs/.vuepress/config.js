3dd8bdeff60099e223b795b0bd43579ac06a5272
zoufeifan <zoufeifan@sensetime.com>
2023-07-13 15:57:18 +0800

    feat: init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+  lang: 'zh-CN',
+  title: '你好， VuePress ！',
+  description: '这是我的第一个 VuePress 站点',
+  head: [['link', { rel: 'icon', href: '/images/logo.png' }]]
+})
---
+++ docs/.vuepress/layouts/Layout.vue
@@ -0,0 +1,19 @@
+<script setup>
+import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue'
+</script>
+
+<template>
+  <ParentLayout>
+    <template #navbar-after>
+      <a href="./">home</a>
+      <span style="display: inline-block; width: 20px;"></span>
+      <a href="./guide">guide</a>
+    </template>
+  </ParentLayout>
+</template>
+
+<style lang="css">
+.my-footer {
+  text-align: center;
+}
+</style>
